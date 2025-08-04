890c162043b8ed554358502211d829df57b374bd
yan pizarro <pizarroestevez2@gmail.como>
2025-08-04 12:46:59 -0300

    Section13 Abstract Class

---
+++ src/main/java/udemy/university/section13/AbstractClass.java
@@ -0,0 +1,60 @@
+package udemy.university.section13;
+
+import java.util.TreeMap;
+
+public class AbstractClass {
+
+    public static void main(String[] args) {
+        GeometricFigure geometricFigureRectangle = new Rectangle();
+        GeometricFigure geometricFigureTriangle = new Triangle();
+
+        geometricFigureRectangle.draw();
+        geometricFigureRectangle.other();
+
+        System.out.println();
+
+        geometricFigureTriangle.draw();
+        geometricFigureTriangle.other();
+
+        System.out.println();
+        Triangle triangle = new Triangle();
+        triangle.draw();
+        triangle.otherTriangle();
+        triangle.other();
+
+
+    }
+}
+
+abstract class GeometricFigure {
+
+    abstract public void draw();
+
+    public void other() {
+        System.out.println("Method from the father class");
+    }
+}
+
+class Rectangle extends GeometricFigure{
+
+    @Override
+    public void draw() {
+        System.out.println("Is a rectangle");
+    }
+
+    public void otherRectangle() {
+        System.out.println("Method from Rectangle Class");
+    }
+}
+
+class Triangle extends GeometricFigure {
+
+    @Override
+    public void draw() {
+        System.out.println("Is a Triangle");
+    }
+
+    public void otherTriangle() {
+        System.out.println("Method from Triangle Class");
+    }
+}
