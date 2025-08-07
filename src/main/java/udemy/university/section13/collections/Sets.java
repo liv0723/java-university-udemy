15a3f5a6e61631dcfb70cdbb634a2f0039bf925c
yan pizarro <pizarroestevez2@gmail.como>
2025-08-07 14:51:38 -0300

    Section13 Set

---
+++ src/main/java/udemy/university/section13/collections/Sets.java
@@ -0,0 +1,29 @@
+package udemy.university.section13.collections;
+
+import java.sql.Array;
+import java.util.*;
+
+public class Sets {
+
+    public static void main(String[] args) {
+        printNamesSet();
+
+    }
+
+    public static void printNamesSet() {
+        Set<String > names = new TreeSet<>();
+        names.add("Juana");
+        names.add("Maria");
+        names.add("Juana");
+
+        names.forEach(System.out::println);
+        System.out.println(names.size());
+        names.remove("Juana");
+        System.out.println(names.size());
+
+        List<Integer> aux = new ArrayList<>();
+        aux.to
+
+
+    }
+}
